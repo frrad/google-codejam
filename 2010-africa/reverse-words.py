e6da481dc46ddcdebfbe29434694057de9572095
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-10 21:50:52 -0700

    solved reverse-words

---
+++ 2010-africa/reverse-words.py
@@ -0,0 +1,16 @@
+import sys
+
+f = open(sys.argv[1], 'r')
+data = f.read().split("\n")
+
+#Ignore first line (number specification)
+data = data[1:]
+
+#Take care of potential trailing newline
+while data[-1] == '':
+    data = data[:-1]
+
+for i, line in enumerate(data):
+    temp = line.split(" ")
+    temp.reverse()
+    print ("Case #%d: " + " ".join(temp)) % (i+1)
